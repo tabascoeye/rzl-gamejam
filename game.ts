4337a76e52b5672e5f1288f7831b6e4d68750601
Matthias Schuetz <mschuetz@gmail.com>
2017-04-30 12:33:36 +0200

    back to arcade physics, arm as separate sprite
    arm is now a separate sprite and immovable (moved manually relative to torso)

--- game.ts
+++ game.ts
@@ -5,7 +5,7 @@ let game: Phaser.Game
 
 export function startgame() {
     console.log("hello startgame")
-    game = new Phaser.Game(1024, 768, Phaser.AUTO, '', { preload: preload, create: create, update: update })
+    game = new Phaser.Game(1024, 768, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render })
 }
 
 function preload() {
@@ -15,21 +15,25 @@ function preload() {
     game.load.image('star', 'assets/mateflasche_scaled.png')
     game.load.image('brownbox', 'stockassets/brownbox.png')
     // game.load.spritesheet('dude', 'stockassets/dude.png', 32, 48)
-    game.load.spritesheet('dude', 'assets/blabber_clones_moving_scaled.png', 512 / 4, 220)
+    game.load.spritesheet('dude', 'assets/blabber624_scaled.png', 444 / 6, 220)
+    game.load.image('arm', 'assets/blabberarm624_scaled.png')
 }
 
 let starSprites: phaser.Group
 
 let platforms: phaser.Group
-let player: phaser.Sprite
+let player: phaser.Group
+let playerTorso: phaser.Sprite
+let playerArm: phaser.Sprite
 let cursors: phaser.CursorKeys
-let brownbox : phaser.Sprite
+let brownbox: phaser.Sprite
+let blabberDirection = -1
 
 function create() {
-    game.physics.startSystem(Phaser.Physics.P2JS)
+    game.physics.startSystem(Phaser.Physics.ARCADE)
 
     let sky = game.add.sprite(0, 0, 'sky')
-    sky.scale.set(2,2)
+    sky.scale.set(2, 2)
 
     platforms = game.add.group()
     platforms.enableBody = true
@@ -42,54 +46,78 @@ function create() {
     for (let i = 0; i < 10; i++) {
         let sprite = starSprites.create(i * 50, 0, 'star')
         game.physics.arcade.enable(sprite);
-        let body: Phaser.Physics.P2.Body = sprite.body
+        let body: Phaser.Physics.Arcade.Body = sprite.body
         body.bounce.y = 0.3;
         body.bounce.x = 0.3;
         body.gravity.y = 300;
         body.collideWorldBounds = true;
     }
-    player = game.add.sprite(32, game.world.height - 150, 'dude')
-    game.physics.arcade.enable(player)
-    let body : Phaser.Physics.Arcade.Body = player.body
-    body.setSize(, body.height)
-    player.body.gravity.y = 300
-    player.body.collideWorldBounds = true
-    player.animations.add('left', [0, 1], 5, true);
-    player.animations.add('right', [2, 3], 5, true);
+
+    player = game.add.group()
+    playerArm = player.create(80, game.world.height - 120, 'arm')
+    game.physics.arcade.enable(playerArm)
+    playerArm.body.immovable = true
+    playerTorso = player.create(132, game.world.height - 150, 'dude')
+    game.physics.arcade.enable(playerTorso)
+    let body: Phaser.Physics.Arcade.Body = playerTorso.body
+    // body.setSize(, body.height)
+    body.gravity.y = 300
+    body.collideWorldBounds = true
+    playerTorso.animations.add('left', [1, 2], 5, true)
+    playerTorso.animations.add('right', [3, 4], 5, true)
+
     brownbox = game.add.sprite(700, game.world.height - 64 - 24, 'brownbox')
     //  Our controls.
     cursors = game.input.keyboard.createCursorKeys();
 }
 
 function update() {
-    game.debug.body(player)
     starSprites.forEach((sprite, platforms, player) => {
-        game.debug.body(sprite)
         let body: Phaser.Physics.Arcade.Body = sprite.body
         game.physics.arcade.collide(sprite, platforms);
         game.physics.arcade.collide(sprite, player);
     }, this, true, platforms, player)
-    //  Collide the player and the stars with the platforms
     game.physics.arcade.collide(player, platforms)
 
     //  Reset the players velocity (movement)
     if (cursors.left.isDown) {
         //  Move to the left
-        player.body.velocity.x = -200;
-        player.animations.play('left');
+        blabberDirection = -1
+        playerArm.body.velocity.x = -200;
+        playerTorso.body.velocity.x = -200;
+        playerTorso.animations.play('left');
     } else if (cursors.right.isDown) {
         //  Move to the right
-        player.body.velocity.x = 200;
-        player.animations.play('right');
+        blabberDirection = 1
+        playerArm.body.velocity.x = 200;
+        playerTorso.body.velocity.x = 200;
+        playerTorso.animations.play('right');
     } else {
         //  Stand still
-        player.animations.stop();
-        if (player.body.velocity.x > 0) {
-            player.frame = 2
+        playerTorso.animations.stop();
+        if (blabberDirection > 0) {
+            playerTorso.frame = 3
         }
-        if (player.body.velocity.x < 0) {
-            player.frame = 0
+        if (blabberDirection < 0) {
+            playerTorso.frame = 0
         }
-        player.body.velocity.x = 0;
+        playerArm.body.velocity.x = 0;
+        playerTorso.body.velocity.x = 0;
     }
+    playerArm.body.velocity = playerTorso.body.velocity
+    if (blabberDirection < 0) {
+        playerArm.position.x = playerTorso.position.x - 50
+        playerArm.position.y = playerTorso.position.y + 55 
+    }
+    if (blabberDirection > 0) {
+        playerArm.position.x = playerTorso.position.x + 40
+        playerArm.position.y = playerTorso.position.y + 55         
+    }
+}
+
+function render() {
+    game.debug.text(`player velocity ${playerTorso.body.velocity}`, 16, 16)
+    game.debug.body(playerTorso)
+    game.debug.body(playerArm)
+    starSprites.forEach(s => game.debug.body(s), this)
 }
