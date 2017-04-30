a81cef3d4599c7c8334ac6721fdc17029043b5a7
¯\_(ツ)_/¯ <tabascoeye@gmail.com>
2017-04-30 23:46:50 +0200

    Game menu draft, about menu scaffolding

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ module Gamejam {
         constructor() {
             super(1024, 768, Phaser.AUTO);
             this.state.add('MainMenu', MainMenu, false);
+            this.state.add('About', About, false);
             this.state.add('Level1', Level1, false);
             this.state.start('MainMenu');
         }
--- menu.ts
+++ menu.ts
@@ -1,17 +1,71 @@
 module Gamejam {
     export class MainMenu extends Phaser.State {
 
-        cursor: Phaser.CursorKeys;
+        cursor: Phaser.CursorKeys
+        blabber: Phaser.Sprite
+        logo: Phaser.Sprite
+        start: Phaser.Button
+        about: Phaser.Button
+        scalesize = .6
+
+        preload() {
+            this.load.image('start', 'assets/Ausrufezeichen.png')
+            this.load.image('about', 'assets/Fragezeichen.png')
+            this.load.image('logo', 'assets/Dienstag.png')
+            this.load.image('blabber','assets/blabber_kopf_scaled.png')
+        }
 
         create() {
-            this.game.debug.text("press any cursor key to start the game", 0, 20, "red");
-            this.cursor = this.game.input.keyboard.createCursorKeys()
+            this.game.physics.startSystem(Phaser.Physics.ARCADE);
+            this.logo = this.add.sprite(0, 0, 'logo')
+            this.logo.scale.setTo(.6,.6)
+            this.logo.anchor.setTo(.5,.5)
+            this.logo.position.x = (this.game.world.height-this.logo.height)/2 + 380
+            this.logo.position.y = (this.game.world.width-this.logo.width)/2 + 80
+
+            this.blabber = this.game.add.sprite(0,0, 'blabber')
+            this.blabber.scale.setTo(.6,.6)
+            this.blabber.anchor.setTo(.5,.65)
+            this.blabber.position.y = (this.game.world.height-400)
+            this.blabber.position.x = (150)
+            this.game.physics.enable(this.blabber, Phaser.Physics.ARCADE);
+            this.blabber.body.angularVelocity = 250
+
+            this.start = this.add.button(560, this.game.world.height - 200,
+                'start', this.go, this, 1, 0, 2);
+            this.start.scale.setTo(.35,.35)
+
+            this.about = this.add.button(260, this.game.world.height - 200,
+                'about', this.aboutGame, this, 1, 0, 2);
+            this.about.scale.setTo(.35,.35)
+
         }
 
         update() {
-            if (this.cursor.down.justUp || this.cursor.up.justUp || this.cursor.left.justUp || this.cursor.right.justUp) {
-                this.game.state.start('Level1', true, false);
+            if (this.blabber.angle > 25) {
+                this.blabber.angle = 20
+                this.blabber.body.angularVelocity *= -1
             }
+            if (this.blabber.angle < -25) {
+                this.blabber.angle = -20
+                this.blabber.body.angularVelocity *= -1
+            }
+            this.logo.scale.setTo(this.scalesize, this.scalesize)
+            this.scalesize -= 0.01
+            if(this.scalesize < .5) {
+                this.scalesize = .6
+            }
+        }
+
+        go() {
+            this.game.state.start('Level1', true, false)
+        }
+
+        aboutGame() {
+            this.game.state.start('About', true, false)
+        }
+
+        render() {
         }
 
     }
